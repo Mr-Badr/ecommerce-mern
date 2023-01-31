d5c9011db6dcf6944eae9ab6ab3967fc31cee8d7
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-01-31 22:01:58 +0100

    Error Handler

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,9 +1,10 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   // First we have to check if the user already exists
   const email = req.body.email;
-  const findUser = await User.findOne({email: email});
+  const findUser = await User.findOne({ email: email });
 
   if (!findUser) {
     // Create a new user
@@ -11,11 +12,16 @@ const createUser = async (req, res) => {
     res.json(newUser);
   } else {
     // User already exists
-    res.json({
+
+    // OLED Syntax
+    /* res.json({
       message: "User already exists",
       success: false,
-    });
-  }
-};
+    }); */
 
-module.exports = {createUser}
+    // New syntax : After using express-async-handler
+    throw new Error("User already exists");
+  }
+});
+
+module.exports = { createUser };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000; // if port is not specified, we will use 4000
@@ -10,13 +11,17 @@ dbConnect();
 
 // use body parser To handle HTTP POST requests in Express.js
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* app.use("/", (req, res) => {
   res.send("i am server.");
 }); */
 
 app.use("/api/user", authRouter);
+
+// This middleware should be after all Routes
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("server is running on port : " + PORT);
