c72ac67d79ccd331ba525395fe9f0f049d9a8fc8
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-02-01 01:31:10 +0100

    Login and register functionality + Authentification using JTW

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const { generateToken } = require("../config/jwtToken");
 
 const createUser = asyncHandler(async (req, res) => {
   // First we have to check if the user already exists
@@ -24,4 +25,23 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createUser };
+// Create login controller
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  // check if user already exists && we send the second argument to userModel file to check the password
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    res.json({
+      _id: findUser?._id,
+      _filename: findUser?._filename,
+      _lastname: findUser?._lastname,
+      _email: findUser?._email,
+      _mobile: findUser?._mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error("Invalid credentials: " + password);
+  }
+});
+
+module.exports = { createUser, loginUserCtrl };
