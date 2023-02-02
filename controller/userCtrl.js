18d675be6b100220dcffb0f0789e7613599e989f
Badr Eddine El-Harchali <mr.elharchali@gmail.com>
2023-02-02 19:14:27 +0100

    Logout functionality

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -87,6 +87,31 @@ const handleRefreshToken = asyncHandler(async (req, res) => {
   });
 });
 
+// Logout Functionality
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie?.refreshToken) {
+    throw new Error("No Refresh Token in Cookies");
+  }
+  const refreshToken = cookie.refreshToken;
+  const user = await User.findOne({ refreshToken });
+  if (!user) {
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+    });
+    return res.sendStatus(204); // Forbidden
+  }
+  await User.findOneAndUpdate(refreshToken, {
+    refreshToken: "",
+  });
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: true,
+  });
+  res.sendStatus(204); // Forbidden
+});
+
 // Update a user
 const updateaUser = asyncHandler(async (req, res) => {
   const { _id } = req.user;
@@ -197,4 +222,5 @@ module.exports = {
   blockUser,
   unblockUser,
   handleRefreshToken,
+  logout,
 };
